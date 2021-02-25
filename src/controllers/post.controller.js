3153c65b0d81ad9b1b134340b8d3fca543177ac6
amir moshfegh <amir.2814@gmail.com>
2021-02-25 08:07:37 +0330

    create post controller

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,32 @@
+const ApiError = require('../utils/ApiError');
+const httpStatus = require('http-status');
+const catchAsync = require('../utils/catchAsync');
+const { Post } = require('../models');
+
+const createPost=catchAsync(async(req, res)=>{
+
+});
+
+const updatePost=catchAsync(async(req, res)=>{
+
+});
+
+const deletePost=catchAsync(async(req, res)=>{
+
+});
+
+const getPost=catchAsync(async(req, res)=>{
+
+});
+
+const getPosts=catchAsync(async(req, res)=>{
+
+});
+
+module.exports={
+    createPost,
+    updatePost,
+    deletePost,
+    getPost,
+    getPosts,
+}
