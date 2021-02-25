0dde82759e4564d62c144f7f193ca74c2622add9
amir moshfegh <amir.2814@gmail.com>
2021-02-25 09:02:23 +0330

    post validation

---
+++ src/validations/post.validation.js
@@ -0,0 +1,54 @@
+const Joi = require('joi');
+const { objectId } = require('./custom.validation');
+
+const createPost = {
+    body: Joi.object().keys({
+        title: Joi.string().required(),
+        content: Joi.string(),
+        tags: Joi.array(),
+        user: Joi.required().custom(objectId),
+    }),
+};
+
+const updatePost = {
+    params: Joi.object().keys({
+        postId: Joi.required().custom(objectId),
+    }),
+    body: Joi.object().keys({
+        title: Joi.string().required(),
+        content: Joi.string(),
+        tags: Joi.array(),
+    })
+        .min(1),
+};
+
+const deletePost = {
+    params: Joi.object().keys({
+        postId: Joi.string().custom(objectId),
+    }),
+};
+
+const getPost = {
+    params: Joi.object().keys({
+        postId: Joi.string().custom(objectId),
+    }),
+};
+
+const getPosts = {
+    query: Joi.object().keys({
+        name: Joi.string(),
+        role: Joi.string(),
+        sortBy: Joi.string(),
+        limit: Joi.number().integer(),
+        page: Joi.number().integer(),
+    }),
+};
+
+module.exports = {
+    createPost,
+    updatePost,
+    deletePost,
+    getPost,
+    getPosts,
+}
+
